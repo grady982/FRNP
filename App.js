c99bb019496429810e97f7bde62a06e3fddcfe81
grady.liu <grady0902@gmail.com>
2024-07-08 22:18:00 +0800

    feat: Flexbox

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your course goal' />
+        <Button title='ADD' />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    flex: 1, 
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomColor: '#cccccc',
+    borderBottomWidth: 1,
+    marginBottom: 24
+  },
+  textInput: {
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%'
+  },
+  goalsContainer: {
+    flex: 4
+  }
 });
