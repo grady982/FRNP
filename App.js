6e0438d1ee3678796a9d4084b35cd88be28e3918
grady.liu <grady.liu@webcomm.com.tw>
2024-07-09 18:41:58 +0800

    Add input handling and button click functionality

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [inputText, setInputText] = useState();
+
+  inputHandler = (text) => {
+    setInputText(text);
+  }
+
+  onAddBtnClick = () => {
+    console.log('show:', inputText);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal' />
-        <Button title='ADD' />
+        <TextInput style={styles.textInput} placeholder='Your course goal' onChangeText={inputHandler} />
+        <Button title='ADD' onPress={onAddBtnClick} />
       </View>
       <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
